f7e977fc09771fd91b2e5150b1dfc02aa14ee40c
Wirus <chcc1023@foxmail.com>
2025-02-10 16:15:59 +0800

    Update EmployeePerformance.tsx

--- components/EmployeePerformance.tsx
+++ components/EmployeePerformance.tsx
@@ -2,6 +2,7 @@
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table"
+//import { colors } from "@/utils/colors"
 import { useState } from "react"
 import { Bar, BarChart, CartesianGrid, Legend, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts"
 
